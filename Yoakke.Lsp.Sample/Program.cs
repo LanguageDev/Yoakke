370edd4d9e5333764bee1bac23d33cf16ab996ef
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-05-31 20:38:24 +0200

    Added empty LSP projects

---
+++ Yoakke.Lsp.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yoakke.Lsp.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
