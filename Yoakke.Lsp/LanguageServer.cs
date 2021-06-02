efd8c4a78d45778e7e81b184f1445853b7cec15f
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-06-02 12:39:52 +0200

    Create LanguageServer.cs

---
+++ Yoakke.Lsp/LanguageServer.cs
@@ -0,0 +1,78 @@
+﻿using StreamJsonRpc;
+using StreamJsonRpc.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yoakke.Lsp.Model.General;
+
+namespace Yoakke.Lsp
+{
+    /// <summary>
+    /// Implements basic LSP server logic.
+    /// </summary>
+    public class LanguageServer
+    {
+        // These should come in strictly increasing order
+        private enum ServerState
+        {
+            NotStarted,
+            WaitingForInitializeRequest,
+            WaitingForInitializedNotification,
+            Operating,
+            ShutdownRequested,
+            ExitRequested,
+        }
+
+        private JsonRpc rpcServer;
+        // TODO: Interlocked?
+        private ServerState state = ServerState.NotStarted;
+
+        public void Start()
+        {
+
+        }
+
+        [JsonRpcMethod("initialize", UseSingleObjectParameterDeserialization = true)]
+        private InitializeResult Initialize(InitializeParams initializeParams)
+        {
+            AssertState(ServerState.WaitingForInitializeRequest, JsonRpcErrorCode.InvalidRequest);
+            // We are waiting for an initialize request and just received one, answer with the proper result
+            var result = new InitializeResult
+            {
+                // TODO
+            };
+            // Increment server state
+            SetServerState(ServerState.WaitingForInitializedNotification);
+            return result;
+        }
+
+        // State modification //////////////////////////////////////////////////
+
+        private void SetServerState(ServerState toState)
+        {
+            if ((int)this.state > (int)toState)
+            {
+                throw new LocalRpcException($"server tried to go backwards in it's state progression")
+                {
+                    ErrorCode = (int)JsonRpcErrorCode.InternalError,
+                };
+            }
+        }
+
+        private void AssertState(ServerState expectedState, JsonRpcErrorCode errorCode) =>
+            AssertState(expectedState, (int)errorCode);
+
+        private void AssertState(ServerState expectedState, int errorCode)
+        {
+            if ((int)this.state != (int)expectedState)
+            {
+                throw new LocalRpcException($"server was not in the expected {expectedState} state for the request")
+                {
+                    ErrorCode = errorCode,
+                };
+            }
+        }
+    }
+}
