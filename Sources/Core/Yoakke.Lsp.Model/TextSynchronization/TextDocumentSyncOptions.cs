d6f1213ba94c9fed31f9855a6752c8c296bd552d
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-06-17 19:02:36 +0200

    Update TextDocumentSyncOptions.cs

--- Sources/Core/Yoakke.Lsp.Model/TextSynchronization/TextDocumentSyncOptions.cs
+++ Sources/Core/Yoakke.Lsp.Model/TextSynchronization/TextDocumentSyncOptions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2021 Yoakke.
+// Copyright (c) 2021 Yoakke.
 // Licensed under the Apache License, Version 2.0.
 // Source repository: https://github.com/LanguageDev/Yoakke
 
@@ -23,5 +23,26 @@ namespace Yoakke.Lsp.Model.TextSynchronization
         /// </summary>
         [JsonProperty("change", NullValueHandling = NullValueHandling.Ignore)]
         public TextDocumentSyncKind? Change { get; set; }
+
+        /// <summary>
+        /// If present will save notifications are sent to the server. If omitted
+        /// the notification should not be sent.
+        /// </summary>
+        [JsonProperty("willSave", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? WillSave { get; set; }
+
+        /// <summary>
+        /// If present will save wait until requests are sent to the server. If
+        /// omitted the request should not be sent.
+        /// </summary>
+        [JsonProperty("willSaveWaitUntil", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? WillSaveWaitUntil { get; set; }
+
+        /// <summary>
+        /// If present save notifications are sent to the server. If omitted the
+        /// notification should not be sent.
+        /// </summary>
+        [JsonProperty("save", NullValueHandling = NullValueHandling.Ignore)]
+        public Either<bool, SaveOptions>? Save { get; set; }
     }
 }
