51f7a02a33a7337b7d361c86b6577c89f7a229ba
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-06-18 21:34:25 +0200

    Removed duplicate

--- Sources/Core/Yoakke.Lsp.Model/Capabilities/Client/TextDocument/PublishDiagnosticsClientCapabilities.cs
+++ Sources/Core/Yoakke.Lsp.Model/Capabilities/Client/TextDocument/PublishDiagnosticsClientCapabilities.cs
@@ -1,10 +1,10 @@
-﻿// Copyright (c) 2021 Yoakke.
+// Copyright (c) 2021 Yoakke.
 // Licensed under the Apache License, Version 2.0.
 // Source repository: https://github.com/LanguageDev/Yoakke
 
 using System.Collections.Generic;
 using Newtonsoft.Json;
-using Yoakke.Lsp.Model.Diagnostics;
+using Yoakke.Lsp.Model.Basic;
 
 namespace Yoakke.Lsp.Model.Capabilities.Client.TextDocument
 {
