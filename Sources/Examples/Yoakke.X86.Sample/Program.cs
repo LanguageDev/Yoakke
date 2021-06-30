360a1bcd2fccc5c3e22cb71f800896fe3fe6b49c
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-06-30 17:07:19 +0200

    Added empty x86 libraries

---
+++ Sources/Examples/Yoakke.X86.Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yoakke.X86.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
