91fe35b00e6ba91620586483c3fb4e1cfb9ab713
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-07-03 12:03:20 +0200

    Added a simple instruction structure

---
+++ Sources/Core/Yoakke.X86/Opcode.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2021 Yoakke.
+// Licensed under the Apache License, Version 2.0.
+// Source repository: https://github.com/LanguageDev/Yoakke
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yoakke.X86
+{
+    /// <summary>
+    /// The different x86 opcodes for instructions.
+    /// </summary>
+    public enum Opcode
+    {
+    }
+}
