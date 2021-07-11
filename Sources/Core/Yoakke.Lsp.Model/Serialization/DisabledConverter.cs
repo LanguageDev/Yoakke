6fa72cd0bb818a319e09b1f578a66eeca0aead74
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-07-11 15:15:57 +0200

    Update DisabledConverter.cs

--- Sources/Core/Yoakke.Lsp.Model/Serialization/DisabledConverter.cs
+++ Sources/Core/Yoakke.Lsp.Model/Serialization/DisabledConverter.cs
@@ -20,21 +20,15 @@ namespace Yoakke.Lsp.Model.Serialization
         public override bool CanWrite => false;
 
         /// <inheritdoc/>
-        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
-        {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) =>
             throw new NotImplementedException();
-        }
 
         /// <inheritdoc/>
-        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
-        {
+        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer) =>
             throw new NotImplementedException();
-        }
 
         /// <inheritdoc/>
-        public override bool CanConvert(Type objectType)
-        {
+        public override bool CanConvert(Type objectType) =>
             throw new NotImplementedException();
-        }
     }
 }
