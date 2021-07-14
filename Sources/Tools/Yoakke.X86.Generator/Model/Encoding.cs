ae89f3fdc3c201691c9698f4d808d230922aecc6
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-07-14 08:04:11 +0200

    Added proper postbyte parsing

--- Sources/Tools/Yoakke.X86.Generator/Model/Encoding.cs
+++ Sources/Tools/Yoakke.X86.Generator/Model/Encoding.cs
@@ -4,9 +4,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 using System.Xml.Serialization;
 
 namespace Yoakke.X86.Generator.Model
@@ -14,7 +18,7 @@ namespace Yoakke.X86.Generator.Model
     /// <summary>
     /// A description on how to encode an <see cref="InstructionForm"/>.
     /// </summary>
-    public class Encoding
+    public class Encoding : IXmlSerializable
     {
         // TODO: This is not correct. The opcode can be followed by a ModRM and then another Opcode
         // So we should only consume consecutive tags here, but I didn't figure out how to do that yet
@@ -24,5 +28,53 @@ namespace Yoakke.X86.Generator.Model
         /// </summary>
         [XmlElement("Opcode")]
         public List<Opcode> Opcodes { get; set; } = new();
+
+        /// <summary>
+        /// The postbyte some instruction encodings require.
+        /// </summary>
+        [XmlElement("Opcode")]
+        public Opcode? Postbyte { get; set; }
+
+        /// <inheritdoc/>
+        public XmlSchema? GetSchema() => null;
+
+        /// <inheritdoc/>
+        public void ReadXml(XmlReader reader)
+        {
+            reader.MoveToContent();
+            var subtree = reader.ReadSubtree();
+            XDocument xDoc = XDocument.Load(subtree);
+
+            var pastOpcode = false;
+            foreach (var element in xDoc.Element("Encoding")!.Elements())
+            {
+                if (element.Name == "Opcode")
+                {
+                    var opcode = Deserialize<Opcode>(element.CreateReader());
+                    if (pastOpcode)
+                    {
+                        Debug.Assert(this.Postbyte is null, "There can only be a single postbyte");
+                        this.Postbyte = opcode;
+                    }
+                    else
+                    {
+                        this.Opcodes.Add(opcode);
+                    }
+                }
+                else
+                {
+                    if (this.Opcodes.Count > 0) pastOpcode = true;
+                }
+            }
+
+            subtree.Close();
+            reader.ReadEndElement();
+        }
+
+        /// <inheritdoc/>
+        public void WriteXml(XmlWriter writer) => throw new NotImplementedException();
+
+        private static T Deserialize<T>(XmlReader reader) =>
+            (T?)new XmlSerializer(typeof(T)).Deserialize(reader) ?? throw new InvalidOperationException();
     }
 }
--- Sources/Tools/Yoakke.X86.Generator/Program.cs
+++ Sources/Tools/Yoakke.X86.Generator/Program.cs
@@ -18,7 +18,7 @@ namespace Yoakke.X86.Generator
             var isa = (InstructionSet?)serializer.Deserialize(new FileStream(@"c:\TMP\x86_gen\Opcodes\opcodes\x86.xml", FileMode.Open, FileAccess.Read));
             if (isa is null) throw new InvalidOperationException();
 
-            var i = isa.Instructions.Where(i => i.Name == "ADD").First();
+            var i = isa.Instructions.Where(i => i.Name == "PAVGUSB").First();
         }
     }
 }
