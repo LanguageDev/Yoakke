e152ec3468c41bec83cb7f02b83812865c659987
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-07-17 10:39:13 +0200

    Added constructor and property generation in the class generator

--- Sources/Tools/Yoakke.X86.Generator/ClassGenerator.cs
+++ Sources/Tools/Yoakke.X86.Generator/ClassGenerator.cs
@@ -17,6 +17,15 @@ namespace Yoakke.X86.Generator
     /// </summary>
     public static class ClassGenerator
     {
+        private struct OperandProperty
+        {
+            public int OperandIndex { get; set; }
+
+            public string Name { get; set; }
+
+            public string Docs { get; set; }
+        }
+
         /// <summary>
         /// Generates code for a single <see cref="Instruction"/> class.
         /// </summary>
@@ -43,13 +52,125 @@ namespace Yoakke.X86.Generator
                 .AppendLine("    /// <summary>")
                 .AppendLine($"    /// The operands of this instruction.")
                 .AppendLine("    /// </summary>");
-            result
-                .AppendLine($"    public IReadOnlyList<IOperand> Operands {{ get; }}")
-                .AppendLine();
+            result.AppendLine($"    public IReadOnlyList<IOperand> Operands {{ get; }}");
+
+            // Infer properties
+            var properties = GenerateProperties(instruction);
+
+            // Write properties into the class
+            foreach (var prop in properties)
+            {
+                result.AppendLine();
+
+                // Doc comment
+                result
+                    .AppendLine("    /// <summary>")
+                    .AppendLine($"    /// {prop.Docs}")
+                    .AppendLine("    /// </summary>");
+                // Getter and setter
+                result
+                    .AppendLine($"    public IOperand {prop.Name}")
+                    .AppendLine("    {")
+                    .AppendLine($"        get => this.Operands[{prop.OperandIndex}];")
+                    .AppendLine($"        set => this.Operands[{prop.OperandIndex}] = value;")
+                    .AppendLine("    }");
+            }
+
+            // Generate the constructors into the class
+            for (var paramCount = 0; paramCount <= properties.Count; ++paramCount)
+            {
+                // Check if we even need a constructor with 'paramCount' no. parameters
+                if (!instruction.Forms.Any(f => f.Operands.Count == paramCount)) continue;
+
+                // Yes we do need it, infer some names for us
+                var ctorParams = GenerateConstructorParams(instruction, paramCount);
+
+                result.AppendLine();
+
+                // Ctor doc comment
+                result
+                    .AppendLine("    /// <summary>")
+                    .AppendLine($"    /// Initializes a new instance of the <see cref=\"{className}\"/> class.")
+                    .AppendLine("    /// </summary>");
+                // Parameter doc comments
+                foreach (var param in ctorParams)
+                {
+                    result.AppendLine($"    /// <param name=\"{param.Name}\">{param.Docs}</param>");
+                }
+
+                // Ctor code
+                result.Append($"    public {className}(");
+                // Parameters
+                var first = true;
+                foreach (var param in ctorParams)
+                {
+                    if (!first) result.Append(", ");
+                    first = false;
+
+                    result.Append($"IOperand {param.Name}");
+                }
+                // Body
+                result
+                    .AppendLine(")")
+                    .AppendLine("    {")
+                    .AppendLine($"        this.Operands = new[] {{ {string.Join(", ", ctorParams.Select(p => p.Name))} }};")
+                    .AppendLine("    }");
+            }
+
+            result.AppendLine("}");
 
             return result.ToString();
         }
 
+        private static IReadOnlyList<OperandProperty> GenerateProperties(Instruction instruction)
+        {
+            var properties = new List<OperandProperty>();
+
+            // Find out how many properties we need
+            var propCount = instruction.Forms.Max(f => f.Operands.Count);
+
+            // TODO: Make this a bit smarter
+            // For now we just generate Operand1, Operand2, ...
+            for (var i = 0; i < propCount; ++i)
+            {
+                properties.Add(new()
+                {
+                    OperandIndex = i,
+                    Name = $"Operand{i + 1}",
+                    Docs = $"The {i + 1}{GetNumberSuffix(i + 1)} operand.",
+                });
+            }
+
+            return properties;
+        }
+
+        private static IReadOnlyList<OperandProperty> GenerateConstructorParams(Instruction instruction, int operandCount)
+        {
+            var properties = new List<OperandProperty>();
+
+            // TODO: Make this a bit smarter
+            // For now we just generate Operand1, Operand2, ...
+            for (var i = 0; i < operandCount; ++i)
+            {
+                properties.Add(new()
+                {
+                    OperandIndex = i,
+                    Name = $"operand{i + 1}",
+                    Docs = $"The {i + 1}{GetNumberSuffix(i + 1)} operand.",
+                });
+            }
+
+            return properties;
+        }
+
         private static string Capitalize(string name) => $"{char.ToUpper(name[0])}{name.Substring(1).ToLower()}";
+
+        private static string GetNumberSuffix(int i) => i switch
+        {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th",
+        };
     }
 }
