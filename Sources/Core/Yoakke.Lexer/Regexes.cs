3e6b97cfffb359376e1076d0d3d9e9e07bcbebfd
SaltyKawaiiNeko <software@skneko.moe>
2021-08-09 17:09:03 +0200

    Add a lexer regex for real number literals.

--- Sources/Core/Yoakke.Lexer/Regexes.cs
+++ Sources/Core/Yoakke.Lexer/Regexes.cs
@@ -30,6 +30,19 @@ namespace Yoakke.Lexer
         public const string HexLiteral = "0x[0-9a-fA-F]+";
 
         /// <summary>
+        /// Decimal number with an optional fractionary part and optional scientific notation.
+        /// </summary>
+        /// <remarks>
+        /// Accepts both decimal numbers that are integers and numbers that have a fractionary part (e.g. both <c>2</c> and <c>2.0</c> are valid).
+        /// If the whole part is zero, it may be omitted (<c>.12</c> means <c>0.12</c>).
+        /// This does not capture the leading sign (<c>+</c> or <c>-</c>), which must be handled separately.
+        /// Scientific notation is supported by writting <c>e</c> or <c>E</c> and then using an integer exponent, which may be optionally negative.
+        ///
+        /// Note that this CANNOT represent all IEEE-754 floating point values, as special values such as infinity or NaN are not supported.
+        /// </remarks>
+        public const string RealNumberLiteral = "(([0-9]*.)|([0-9]+[0-9_]*.))?[0-9]+[0-9_]*((e|E)-?[0-9]+)?";
+
+        /// <summary>
         /// A single-line string.
         /// </summary>
         public const string StringLiteral = @"""((\\[^\n\r])|[^\r\n\\""])*""";
