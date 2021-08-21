b770726249f2aa88f808b9c3fa23eb36a602e028
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-08-21 20:56:46 +0200

    Added a polyfill library for .netstandard2.0 fillers

---
+++ Sources/Core/Yoakke.Polyfill/KeyValuePairExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yoakke.Polyfill
+{
+    /// <summary>
+    /// Extensions for <see cref="KeyValuePair{TKey, TValue}"/>.
+    /// </summary>
+    public static class KeyValuePairExtensions
+    {
+        /// <summary>
+        /// Deconstructs the current <see cref="KeyValuePair{TKey, TValue}"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="keyValuePair">The <see cref="KeyValuePair{TKey, TValue}"/> to deconstruct.</param>
+        /// <param name="key">The key of the current <see cref="KeyValuePair{TKey, TValue}"/>.</param>
+        /// <param name="value">The value of the current <see cref="KeyValuePair{TKey, TValue}"/>.</param>
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> keyValuePair, out TKey key, out TValue value)
+        {
+            key = keyValuePair.Key;
+            value = keyValuePair.Value;
+        }
+    }
+}
