35632200efb2f2e3927858e80a75a0e1c4fb8a31
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-09-15 14:44:12 +0200

    Update DenseSet.cs

--- Sources/Core/Yoakke.Collections/Dense/DenseSet.cs
+++ Sources/Core/Yoakke.Collections/Dense/DenseSet.cs
@@ -89,7 +89,40 @@ namespace Yoakke.Collections.Dense
                 return true;
             }
 
-            throw new NotImplementedException();
+            // Not empty, find all the intervals that are touched
+            var (from, to) = this.TouchingRange(interval);
+
+            if (from == to)
+            {
+                // Just insert, touches nothing
+                this.intervals.Insert(from, interval);
+                return true;
+            }
+            else
+            {
+                // We need to remove all touched intervals
+                // First we need to modify the inserted interval, because the touched intervals might extend beyond
+                // the inserted one
+                var firstLower = this.intervals[from].Lower;
+                var lastUpper = this.intervals[to - 1].Upper;
+                var minLower = this.Comparer.BoundComparer.Min(firstLower, interval.Lower);
+                var maxUpper = this.Comparer.BoundComparer.Max(lastUpper, interval.Upper);
+                // There are 3 ways we can cover a new value
+                //  - There are more than 1 touched intervals, because that means we fill out values in between
+                //  - The first touched intervals lower value is below the inserted one
+                //  - The last touched intervals upper value is above the inserted one
+                var coversNewValue = to - from > 1
+                                 || !this.Comparer.BoundComparer.Equals(minLower, interval.Lower)
+                                 || !this.Comparer.BoundComparer.Equals(maxUpper, interval.Upper);
+                if (!coversNewValue) return false;
+                // It covers something new, we need to do the insertion
+                interval = new(minLower, maxUpper);
+                // Remove all touched ranges, except the first one
+                this.intervals.RemoveRange(from + 1, to - from - 1);
+                // We modify the first, not removed touched range to save on memory juggling a bit
+                this.intervals[from] = interval;
+                return true;
+            }
         }
 
         /// <inheritdoc/>
@@ -192,5 +225,40 @@ namespace Yoakke.Collections.Dense
 
         /// <inheritdoc/>
         IEnumerator IEnumerable.GetEnumerator() => (this.intervals as IEnumerable).GetEnumerator();
+
+        private (int From, int To) TouchingRange(Interval<T> interval)
+        {
+            var (from, to) = this.IntersectingRange(interval);
+            if (from != 0 && this.Comparer.BoundComparer.IsTouching(interval.Lower, this.intervals[from - 1].Upper)) from -= 1;
+            if (to != this.intervals.Count && this.Comparer.BoundComparer.IsTouching(interval.Upper, this.intervals[to].Lower)) to += 1;
+            return (from, to);
+        }
+
+        private (int From, int To) IntersectingRange(Interval<T> interval)
+        {
+            var from = this.BinarySearch(0, interval.Lower, iv => iv.Upper);
+            var to = this.BinarySearch(from, interval.Upper, iv => iv.Lower);
+            return (from, to);
+        }
+
+        private int BinarySearch(int start, Bound<T> searchedKey, Func<Interval<T>, Bound<T>> keySelector)
+        {
+            var size = this.intervals.Count - start;
+            if (size == 0) return 0;
+
+            while (size > 1)
+            {
+                var half = size / 2;
+                var mid = start + half;
+                var key = keySelector(this.intervals[mid]);
+                var cmp = this.Comparer.BoundComparer.Compare(searchedKey, key);
+                start = cmp > 0 ? mid : start;
+                size -= half;
+            }
+
+            var resultKey = keySelector(this.intervals[start]);
+            var resultCmp = this.Comparer.BoundComparer.Compare(searchedKey, resultKey);
+            return start + (resultCmp > 0 ? 1 : 0);
+        }
     }
 }
