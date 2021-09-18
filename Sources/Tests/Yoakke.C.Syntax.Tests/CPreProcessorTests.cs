12e9dd95868af8f0a6329addcf6e50a4289e03b4
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-09-18 22:35:47 +0200

    Cleaned up C syntax tests

--- Sources/Tests/Yoakke.C.Syntax.Tests/CPreProcessorTests.cs
+++ Sources/Tests/Yoakke.C.Syntax.Tests/CPreProcessorTests.cs
@@ -9,100 +9,79 @@ namespace Yoakke.C.Syntax.Tests
 {
     public class CPreProcessorTests
     {
-        public static IEnumerable<object[]> TextEqualsInputs { get; } = new object[][]
-        {
-            TextInput(
-                "a b c",
-                "a b c"),
-
-            TextInput(
+        [Theory]
+        [InlineData(
+"a b c",
+"a b c")]
+        [InlineData(
 @"#define FOO
 FOO",
-string.Empty),
-
-            TextInput(
+"")]
+        [InlineData(
 @"#define FOO BAR
 FOO",
-"BAR"),
-
-            TextInput(
+"BAR")]
+        [InlineData(
 @"#define FOO a b c
 FOO",
-"a b c"),
-
-            TextInput(
+"a b c")]
+        [InlineData(
 @"#define FOO BAR
 #define BAR QUX
 FOO",
-"QUX"),
-
-            TextInput(
+"QUX")]
+        [InlineData(
 @"#define FOO(x) a x b
 FOO(abc)",
-"a abc b"),
+"a abc b")]
 
-            TextInput(
+        [InlineData(
 @"#define FOO(x) a x b
 FOO((x, y, z))",
-"a (x, y, z) b"),
-
-            TextInput(
+"a (x, y, z) b")]
+        [InlineData(
 @"#define CAT(a, b) a ## b
 CAT(ab, 2)
 CAT(L, ""asd"")",
-"ab2 L\"asd\""),
-
-            TextInput(
+"ab2 L\"asd\"")]
+        [InlineData(
 @"#define FOO() A
 #define LP() (
 #define RP() )
 FOO LP() RP()",
-"FOO ( )"),
-
-            TextInput(
+"FOO ( )")]
+        [InlineData(
 @"#define FOO() A
 #define EXPAND(x) x
 #define LP() (
 #define RP() )
 EXPAND(FOO LP() RP())",
-"A"),
-
-            TextInput(
+"A")]
+        [InlineData(
 @"#define FOO(...) __VA_ARGS__
 FOO(a, b, c d)",
-"a, b, c d"),
-
-            TextInput(
+"a, b, c d")]
+        [InlineData(
 @"#define FOO(...) #__VA_ARGS__
 FOO(a, b, c)",
-"\"a, b, c\""),
-
-            TextInput(
+"\"a, b, c\"")]
+        [InlineData(
 @"#define FOO(...) #__VA_ARGS__
 FOO(a, b,c)",
-"\"a, b,c\""),
-
-            TextInput(
+"\"a, b,c\"")]
+        [InlineData(
 @"#define FOO(...) #__VA_ARGS__
 FOO()",
-"\"\""),
-
-            TextInput(
+"\"\"")]
+        [InlineData(
 @"#define FOO(x, ...) x
 FOO(hello)",
-"hello"),
-
-            TextInput(
+"hello")]
+        [InlineData(
 @"__COUNTER__
 __COUNTER__
 __COUNTER__",
-"0 1 2"),
-        };
-
-        private static object[] TextInput(string beforePP, string afterPP) => new object[] { beforePP, afterPP };
-
-        [Theory]
-        [MemberData(nameof(TextEqualsInputs))]
+"0 1 2")]
         public void TextEquals(string beforePP, string afterPP)
         {
             var expectLexer = new CLexer(afterPP);
