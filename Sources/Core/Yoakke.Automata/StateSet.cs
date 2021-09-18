4b439e5a89836deb41cc179bd505b574b1af1c75
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-09-18 14:23:39 +0200

    Update StateSet.cs

--- Sources/Core/Yoakke.Automata/StateSet.cs
+++ Sources/Core/Yoakke.Automata/StateSet.cs
@@ -3,6 +3,7 @@
 // Source repository: https://github.com/LanguageDev/Yoakke
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,9 +14,49 @@ namespace Yoakke.Automata
     /// Useful for keeping readability.
     /// </summary>
     /// <typeparam name="TState">The state type.</typeparam>
-    public sealed record StateSet<TState>(ISet<TState> States)
+    public sealed class StateSet<TState> : IReadOnlyCollection<TState>, IEquatable<StateSet<TState>>
     {
         /// <inheritdoc/>
-        public override string ToString() => string.Join(", ", this.States);
+        public int Count => this.states.Count;
+
+        private readonly HashSet<TState> states;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StateSet{TState}"/> class.
+        /// </summary>
+        /// <param name="states">The states this set consists of.</param>
+        /// <param name="equalityComparer">The comparer to use.</param>
+        public StateSet(IEnumerable<TState> states, IEqualityComparer<TState> equalityComparer)
+        {
+            this.states = new(states, equalityComparer);
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) => obj is StateSet<TState> other && this.Equals(other);
+
+        /// <inheritdoc/>
+        public bool Equals(StateSet<TState> other)
+        {
+            if (!this.states.Comparer.Equals(other.states.Comparer)) throw new InvalidOperationException("The comparer of the two state set are different");
+            return this.states.SetEquals(other.states);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            var h = 0;
+            // NOTE: We use XOR to have order-independent hashing
+            foreach (var s in this.states) h ^= this.states.Comparer.GetHashCode(s);
+            return h;
+        }
+
+        /// <inheritdoc/>
+        public override string ToString() => string.Join(", ", this.states);
+
+        /// <inheritdoc/>
+        public IEnumerator<TState> GetEnumerator() => this.states.GetEnumerator();
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator() => (this.states as IEnumerable).GetEnumerator();
     }
 }
