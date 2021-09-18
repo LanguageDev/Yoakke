5723a3d28fd00dcb84b10b7355b810cff15377a2
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-09-18 10:16:49 +0200

    Added base interfaces for finite automatas

---
+++ Sources/Core/Yoakke.Automata/IFiniteAutomaton.cs
@@ -0,0 +1,46 @@
+// Copyright (c) 2021 Yoakke.
+// Licensed under the Apache License, Version 2.0.
+// Source repository: https://github.com/LanguageDev/Yoakke
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yoakke.Automata
+{
+    /// <summary>
+    /// Represents a generic finite automaton.
+    /// </summary>
+    /// <typeparam name="TState">The state type.</typeparam>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    public interface IFiniteAutomaton<TState, TSymbol> : IReadOnlyFiniteAutomaton<TState, TSymbol>
+    {
+        /// <summary>
+        /// The initial state of the automaton.
+        /// </summary>
+        public new TState InitialState { get; set; }
+
+        /// <summary>
+        /// The accepting states of the automaton.
+        /// </summary>
+        public new ICollection<TState> AcceptingStates { get; }
+
+        /// <summary>
+        /// Adds a transition to this automaton.
+        /// </summary>
+        /// <param name="from">The state to transition from.</param>
+        /// <param name="on">The symbol that triggers this transition.</param>
+        /// <param name="to">The state to transition to.</param>
+        /// <returns>True, if the transition was new and successfully added.</returns>
+        public bool AddTransition(TState from, TSymbol on, TState to);
+
+        /// <summary>
+        /// Removes a transition from this automaton.
+        /// </summary>
+        /// <param name="from">The state to transition from.</param>
+        /// <param name="on">The symbol that triggers this transition.</param>
+        /// <param name="to">The state to transition to.</param>
+        /// <returns>True, if the transition was found and removed, false otherwise.</returns>
+        public bool RemoveTransition(TState from, TSymbol on, TState to);
+    }
+}
---
+++ Sources/Core/Yoakke.Automata/IReadOnlyFiniteAutomaton.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yoakke.Automata
+{
+    /// <summary>
+    /// Represents a generic, read-only finite automaton.
+    /// </summary>
+    /// <typeparam name="TState">The state type.</typeparam>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    public interface IReadOnlyFiniteAutomaton<TState, TSymbol>
+    {
+        /// <summary>
+        /// The initial state of the automaton.
+        /// </summary>
+        public TState InitialState { get; }
+
+        /// <summary>
+        /// The accepting states of the automaton.
+        /// </summary>
+        public IReadOnlyCollection<TState> AcceptingStates { get; }
+
+        /// <summary>
+        /// All states of the automaton.
+        /// </summary>
+        public IEnumerable<TState> States { get; }
+
+        /// <summary>
+        /// Checks if this automaton results in an accepting state for the given input.
+        /// </summary>
+        /// <param name="input">The sequence of input to feed in for the automaton.</param>
+        /// <returns>True, if the automaton accepts the input, false otherwise.</returns>
+        public bool Accepts(IEnumerable<TSymbol> input);
+
+        /// <summary>
+        /// Creates a Graphviz DOT code representation of this automata for visualization.
+        /// </summary>
+        /// <returns>The full graphviz code.</returns>
+        public string ToDot();
+    }
+}
