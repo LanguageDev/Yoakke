366c2b9af514a09d408dc4d56475bf8c1c53e88f
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-09-18 12:59:41 +0200

    Update Program.cs

--- Sources/Examples/Yoakke.Automata.Sample/Program.cs
+++ Sources/Examples/Yoakke.Automata.Sample/Program.cs
@@ -7,23 +7,29 @@ namespace Yoakke.Automata.Sample
     {
         static void Main(string[] args)
         {
-            var dfa = new Dfa<char, char>();
-            dfa.AcceptingStates.Add('q');
-            dfa.InitialState = 'q';
-            dfa.AddTransition('q', 'a', 'q');
-            dfa.Complete("ab", 't');
-            Console.WriteLine(dfa.ToDot());
+            var dfa = new Dfa<string, char>();
 
-            dfa.AddTransition('c', 'a', 'd');
-            dfa.RemoveUnreachable();
-            Console.WriteLine(dfa.ToDot());
+            dfa.AddTransition("S", 'a', "A");
+            dfa.AddTransition("S", 'b', "B");
+            dfa.AddTransition("B", 'b', "BB");
+            dfa.AddTransition("BB", 'b', "BB");
+            dfa.AddTransition("BB", 'a', "BA");
+            dfa.AddTransition("B", 'a', "BA");
+            dfa.AddTransition("A", 'a', "AA");
+            dfa.AddTransition("A", 'b', "AB");
+            dfa.AddTransition("AA", 'a', "AA");
+            dfa.AddTransition("AA", 'b', "AB");
+            dfa.AddTransition("AB", 'a', "BA");
+            dfa.AddTransition("AB", 'b', "BB");
+            dfa.AddTransition("BA", 'a', "AA");
+            dfa.AddTransition("BA", 'b', "AB");
 
-            while (true)
-            {
-                var line = Console.ReadLine();
-                if (line is null) break;
-                Console.WriteLine(dfa.Accepts(line));
-            }
+            dfa.AcceptingStates.Add("AB");
+            dfa.AcceptingStates.Add("BA");
+
+            dfa.InitialState = "S";
+
+            Console.WriteLine(dfa.ToDot());
         }
     }
 }
