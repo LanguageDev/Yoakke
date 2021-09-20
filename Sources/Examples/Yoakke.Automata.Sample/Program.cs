3d331213b809e50dd753f6bd10c46eb1c8f198e2
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-09-20 10:59:14 +0200

    Update Program.cs

--- Sources/Examples/Yoakke.Automata.Sample/Program.cs
+++ Sources/Examples/Yoakke.Automata.Sample/Program.cs
@@ -1,10 +1,22 @@
 using System;
-using Yoakke.Automata.Discrete;
+using Yoakke.Automata.Sparse;
 
 namespace Yoakke.Automata.Sample
 {
     class Program
     {
+        /*
+         TODO: what we could do still
+          - Improve the StateSet interface (see the note there)
+          - The I(ReadOnly)SparseAutomata interfaces could get ISparseNfa and Dfa pairs
+            (these are just essentially convenience interfaces)
+            IReadOnlySparseDfa, ISparseDfa, IReadOnlySparseNfa, ISparseNfa
+          - Move initial state to the NFA and DFA interfaces, to allow NFA to have multiple initial states
+          - Implement the sparse automata interface for the NFA too
+          - Implement dense automata representations
+          - Move symbol comparer to be required in the interfaces too
+         */
+
         static void Main(string[] args)
         {
             var nfa = new Nfa<string, char>();
