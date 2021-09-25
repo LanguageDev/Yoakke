7cb563673b1f478a9c73ea89e86f583ae75e92d7
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-09-25 14:23:21 +0200

    Factored out a DFA minimizer helper

--- Sources/Core/Yoakke.Automata/Sparse/Dfa.cs
+++ Sources/Core/Yoakke.Automata/Sparse/Dfa.cs
@@ -372,84 +372,34 @@ namespace Yoakke.Automata.Sparse
             IStateCombiner<TState, TResultState> combiner,
             IEnumerable<(TState, TState)> differentiatePairs)
         {
-            var result = new Dfa<TResultState, TSymbol>(combiner.ResultComparer, this.SymbolComparer);
-            var tupleComparer = new TupleEqualityComparer<TState, TState>(this.StateComparer, this.StateComparer);
-            var table = new HashSet<(TState, TState)>(tupleComparer);
+            var equivalenceTable = new EquivalenceTable<TState, TSymbol>(this);
 
-            void Plot(TState s1, TState s2)
-            {
-                table!.Add((s1, s2));
-                table!.Add((s2, s1));
-            }
+            // Initially fill the table
+            equivalenceTable.Initialize(differentiatePairs);
 
-            // First, all (accepting, non-accepting) pairs get plotted in the table
-            var states = this.States.ToList();
-            for (var i = 0; i < states.Count; ++i)
+            // Fill until no change
+            equivalenceTable.Fill((s1, s2) =>
             {
-                var s1 = states[i];
-                for (var j = 0; j < i; ++j)
+                var onSet = (this.transitions.TransitionMap.TryGetValue(s1, out var s1on) ? s1on.Keys : Enumerable.Empty<TSymbol>())
+                    .Concat(this.transitions.TransitionMap.TryGetValue(s2, out var s2on) ? s2on.Keys : Enumerable.Empty<TSymbol>())
+                    .ToHashSet(this.SymbolComparer);
+                foreach (var on in onSet)
                 {
-                    var s2 = states[j];
-                    if (this.AcceptingStates.Contains(s1) != this.AcceptingStates.Contains(s2)) Plot(s1, s2);
+                    var on1has = s1on.TryGetValue(on, out var to1);
+                    var on2has = s2on.TryGetValue(on, out var to2);
+                    if ((on1has && !on2has && this.AcceptingStates.Contains(to1))
+                    || (!on1has && on2has && this.AcceptingStates.Contains(to2))) return true;
+                    if (!on1has || !on2has) continue;
+                    if (equivalenceTable.AreDifferent(to1, to2)) return true;
                 }
-            }
+                return false;
+            });
 
-            // Then we plot the custom pairs too
-            foreach (var (s1, s2) in differentiatePairs) Plot(s1, s2);
-
-            // Now for each (p, q) pair of states
-            //   If (p, q) is unplotted and exists a symbol X, so that (delta(p, X), delta(q, X)) is not empty
-            //     plot (p, q)
-            // Repeat until there is no change
-            while (true)
-            {
-                var changed = false;
-                for (var i = 0; i < states.Count; ++i)
-                {
-                    var s1 = states[i];
-                    for (var j = 0; j < i; ++j)
-                    {
-                        var s2 = states[j];
-
-                        if (table.Contains((s1, s2))) continue;
-
-                        // TODO: Is this correct? If one can but the other can't transition, they are probably not equivalent, as the DFA would fail for one?
-                        if (!this.transitions.TransitionMap.TryGetValue(s1, out var s1on)
-                         || !this.transitions.TransitionMap.TryGetValue(s2, out var s2on)) continue;
-
-                        var onSet = s1on.Keys.Concat(s2on.Keys).ToHashSet(this.SymbolComparer);
-
-                        foreach (var on in onSet)
-                        {
-                            // TODO: Same as above, correctness
-                            if (!s1on.TryGetValue(on, out var to1) || !s2on.TryGetValue(on, out var to2)) continue;
-                            if (table.Contains((to1, to2)))
-                            {
-                                changed = true;
-                                Plot(s1, s2);
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (!changed) break;
-            }
-
-            // Now we have a table that is empty, where two states are equivalent
             // Create a state mapping of old-state -> equivalent-state
-            var stateMap = new Dictionary<TState, TResultState>(this.StateComparer);
-            for (var i = 0; i < states.Count; ++i)
-            {
-                var s1 = states[i];
-                var equivalentSet = new HashSet<TState>(this.StateComparer) { s1 };
-                for (var j = 0; j < states.Count; ++j)
-                {
-                    if (i == j) continue;
-                    var s2 = states[j];
-                    if (!table.Contains((s1, s2))) equivalentSet.Add(s2);
-                }
-                stateMap.Add(s1, combiner.Combine(equivalentSet));
-            }
+            var stateMap = equivalenceTable.BuildStateMap(combiner);
+
+            // Create the result
+            var result = new Dfa<TResultState, TSymbol>(combiner.ResultComparer, this.SymbolComparer);
 
             // Now build the new transitions with the state equivalences
             foreach (var (from, on, to) in this.Transitions) result.AddTransition(stateMap[from], on, stateMap[to]);
