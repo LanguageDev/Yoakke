e7c245a552074ca9a346129989efee0dcdcc31be
Lenkefi Péter <lenkefi.peti@gmail.com>
2022-03-28 19:03:03 +0200

    Continued the PCRE AST, asses regex builders

--- Sources/SynKit/Libraries/RegExes/PcreAst.cs
+++ Sources/SynKit/Libraries/RegExes/PcreAst.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Yoakke.SynKit.RegExes;
@@ -27,18 +28,8 @@ public abstract record class PcreAst
     public sealed record class Alternation(IReadOnlyList<PcreAst> Elements) : PcreAst
     {
         /// <inheritdoc/>
-        public override RegExAst<char> ToPlainRegex(RegExSettings settings)
-        {
-            if (this.Elements.Count == 0) return RegExAst<char>.Epsilon.Instance;
-
-            var result = this.Elements[0].ToPlainRegex(settings);
-            for (var i = 1; i < this.Elements.Count; ++i)
-            {
-                var right = this.Elements[i].ToPlainRegex(settings);
-                result = new RegExAst<char>.Alternation(result, right);
-            }
-            return result;
-        }
+        public override RegExAst<char> ToPlainRegex(RegExSettings settings) =>
+            RegExAst.Alternation(this.Elements.Select(e => e.ToPlainRegex(settings)));
     }
 
     /// <summary>
@@ -48,18 +39,8 @@ public abstract record class PcreAst
     public sealed record class Sequence(IReadOnlyList<PcreAst> Elements) : PcreAst
     {
         /// <inheritdoc/>
-        public override RegExAst<char> ToPlainRegex(RegExSettings settings)
-        {
-            if (this.Elements.Count == 0) return RegExAst<char>.Epsilon.Instance;
-
-            var result = this.Elements[0].ToPlainRegex(settings);
-            for (var i = 1; i < this.Elements.Count; ++i)
-            {
-                var right = this.Elements[i].ToPlainRegex(settings);
-                result = new RegExAst<char>.Sequence(result, right);
-            }
-            return result;
-        }
+        public override RegExAst<char> ToPlainRegex(RegExSettings settings) =>
+            RegExAst.Sequence(this.Elements.Select(e => e.ToPlainRegex(settings)));
     }
 
     /// <summary>
@@ -70,6 +51,81 @@ public abstract record class PcreAst
     public sealed record class Quantified(PcreAst Element, Quantifier Quantifier) : PcreAst
     {
         /// <inheritdoc/>
+        public override RegExAst<char> ToPlainRegex(RegExSettings settings)
+        {
+            var element = this.Element.ToPlainRegex(settings);
+            return this.Quantifier switch
+            {
+                Quantifier.Optional => RegExAst.Option(element),
+                Quantifier.OneOrMore => RegExAst.Repeat1(element),
+                Quantifier.ZeroOrMore => RegExAst.Repeat0(element),
+                Quantifier.Exactly e => RegExAst.Exactly(element, e.Amount),
+                Quantifier.AtLeast l => RegExAst.AtLeast(element, l.Amount),
+                Quantifier.AtMost m => RegExAst.AtMost(element, m.Amount),
+                Quantifier.Between b => RegExAst.Between(element, b.Min, b.Max),
+                _ => throw new InvalidOperationException(),
+            };
+        }
+    }
+
+    /// <summary>
+    /// Represents a character literal.
+    /// </summary>
+    /// <param name="Char">The represented character.</param>
+    public sealed record class Literal(char Char) : PcreAst
+    {
+        /// <inheritdoc/>
         public override RegExAst<char> ToPlainRegex(RegExSettings settings) => throw new NotImplementedException();
     }
+
+    /// <summary>
+    /// Represents some meta-sequence.
+    /// </summary>
+    /// <param name="Id">The identifying object that identifies the meta-sequence.</param>
+    public sealed record class MetaSequence(object Id) : PcreAst
+    {
+        /// <inheritdoc/>
+        public override RegExAst<char> ToPlainRegex(RegExSettings settings) => throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Represents a named character class.
+    /// </summary>
+    /// <param name="Invert">True, if the class should be inverted.</param>
+    /// <param name="Name">The name of the character class.</param>
+    public sealed record class NamedCharacterClass(bool Invert, string Name) : PcreAst
+    {
+        /// <inheritdoc/>
+        public override RegExAst<char> ToPlainRegex(RegExSettings settings) => throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Represents a custom character class.
+    /// </summary>
+    /// <param name="Invert">True, if the class should be inverted.</param>
+    /// <param name="Elements">The elements of the character class.</param>
+    public sealed record class CharacterClass(bool Invert, IReadOnlyList<CharacterClassElement> Elements) : PcreAst
+    {
+        /// <inheritdoc/>
+        public override RegExAst<char> ToPlainRegex(RegExSettings settings) => throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// An element in a custom character class.
+    /// </summary>
+    public abstract record class CharacterClassElement;
+
+    /// <summary>
+    /// A single element in a character class.
+    /// </summary>
+    /// <param name="Ast">The PCRE expressing the single literal.</param>
+    public sealed record class CharacterClassLiteral(PcreAst Ast) : CharacterClassElement;
+
+    /// <summary>
+    /// A range of elements in a character class. It can also be coincidental, syntactical match, then
+    /// it is simply From or '-' or To.
+    /// </summary>
+    /// <param name="From">The lower end of the character range.</param>
+    /// <param name="To">The upper end of the character range.</param>
+    public sealed record class CharacterClassRange(PcreAst From, PcreAst To) : CharacterClassElement;
 }
--- Sources/SynKit/Libraries/RegExes/RegExAst.cs
+++ Sources/SynKit/Libraries/RegExes/RegExAst.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0.
 // Source repository: https://github.com/LanguageDev/Yoakke
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Yoakke.Collections.Intervals;
 using Yoakke.SynKit.Automata;
 
@@ -167,5 +169,168 @@ public abstract record class RegExAst<TSymbol>
 
             return (start, end);
         }
+    }
+}
+
+/// <summary>
+/// Factory functions for regexes.
+/// </summary>
+public static class RegExAst
+{
+    /// <summary>
+    /// Creates alternatives from the given regex nodes.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="alts">The elements to turn into alternatives.</param>
+    /// <returns>A regex that has <paramref name="alts"/> as alternatives.</returns>
+    public static RegExAst<TSymbol> Alternation<TSymbol>(params RegExAst<TSymbol>[] alts) =>
+        Alternation(alts.AsEnumerable());
+
+    /// <summary>
+    /// Creates alternatives from the given regex nodes.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="alts">The elements to turn into alternatives.</param>
+    /// <returns>A regex that has <paramref name="alts"/> as alternatives.</returns>
+    public static RegExAst<TSymbol> Alternation<TSymbol>(IEnumerable<RegExAst<TSymbol>> alts) =>
+        ConstructFolded(alts, (x, y) => new RegExAst<TSymbol>.Alternation(x, y));
+
+    /// <summary>
+    /// Creates a sequence from the given regex nodes.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="elements">The constructs to put in the sequence.</param>
+    /// <returns>A regex that has <paramref name="elements"/> in sequence.</returns>
+    public static RegExAst<TSymbol> Sequence<TSymbol>(params RegExAst<TSymbol>[] elements) =>
+        Sequence(elements.AsEnumerable());
+
+    /// <summary>
+    /// Creates a sequence from the given regex nodes.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="elements">The constructs to put in the sequence.</param>
+    /// <returns>A regex that has <paramref name="elements"/> in sequence.</returns>
+    public static RegExAst<TSymbol> Sequence<TSymbol>(IEnumerable<RegExAst<TSymbol>> elements) =>
+        ConstructFolded(elements, (x, y) => new RegExAst<TSymbol>.Sequence(x, y));
+
+    /// <summary>
+    /// Creates a repetition node from the given construct that repeats 0-or-more times.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="element">The construct to repeat.</param>
+    /// <returns>A regex that repeats <paramref name="element"/> 0 or more times.</returns>
+    public static RegExAst<TSymbol> Repeat0<TSymbol>(RegExAst<TSymbol> element) =>
+        new RegExAst<TSymbol>.Repetition(element);
+
+    /// <summary>
+    /// Creates an optional from a given construct.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="element">The construct to make optional.</param>
+    /// <returns>A regex that makes <paramref name="element"/> optional.</returns>
+    public static RegExAst<TSymbol> Option<TSymbol>(RegExAst<TSymbol> element) =>
+        AtMost(element, 1);
+
+    /// <summary>
+    /// Creates a repetition node from the given construct that repeats 1-or-more times.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="element">The construct to repeat.</param>
+    /// <returns>A regex that repeats <paramref name="element"/> 1 or more times.</returns>
+    public static RegExAst<TSymbol> Repeat1<TSymbol>(RegExAst<TSymbol> element) =>
+        Sequence(element, Repeat0(element));
+
+    /// <summary>
+    /// Creates a repetition node from the given construct that repeats exactly the given amount of times.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="element">The construct to repeat.</param>
+    /// <param name="n">The amount to repeat.</param>
+    /// <returns>A regex that repeats <paramref name="element"/> exactly <paramref name="n"/> times.</returns>
+    public static RegExAst<TSymbol> Exactly<TSymbol>(RegExAst<TSymbol> element, int n)
+    {
+        if (n < 0) throw new ArgumentOutOfRangeException(nameof(n));
+        if (n == 0) return Empty<TSymbol>();
+        var result = element;
+        for (var i = 1; i < n; ++i) result = Sequence(result, element);
+        return result;
+    }
+
+    /// <summary>
+    /// Creates a repetition node from the given construct that repeats at least the given amount of times.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="element">The construct to repeat.</param>
+    /// <param name="n">The amount to repeat at least.</param>
+    /// <returns>A regex that repeats <paramref name="element"/> at least <paramref name="n"/> times.</returns>
+    public static RegExAst<TSymbol> AtLeast<TSymbol>(RegExAst<TSymbol> element, int n) =>
+        Sequence(Exactly(element, n), Repeat0(element));
+
+    /// <summary>
+    /// Creates a repetition node from the given construct that repeats at most the given amount of times.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="element">The construct to repeat.</param>
+    /// <param name="n">The amount to repeat at most.</param>
+    /// <returns>A regex that repeats <paramref name="element"/> at most <paramref name="n"/> times.</returns>
+    public static RegExAst<TSymbol> AtMost<TSymbol>(RegExAst<TSymbol> element, int n) =>
+        Between(element, 0, n);
+
+    /// <summary>
+    /// Creates a repetition node from the given construct that repeats between given amounts of times.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="element">The construct to repeat.</param>
+    /// <param name="from">The amount to repeat at least.</param>
+    /// <param name="to">The amount to repeat at most.</param>
+    /// <returns>A regex that repeats <paramref name="element"/> at least <paramref name="from"/>
+    /// and at most <paramref name="to"/> times.</returns>
+    public static RegExAst<TSymbol> Between<TSymbol>(RegExAst<TSymbol> element, int from, int to)
+    {
+        if (from < 0) throw new ArgumentOutOfRangeException(nameof(from));
+        if (to < from) throw new ArgumentOutOfRangeException(nameof(to));
+
+        var result = Exactly(element, from);
+        for (var i = from + 1; i <= to; ++i) result = Alternation(result, Exactly(element, i));
+        return result;
+    }
+
+    /// <summary>
+    /// Creates a node that represents an empty word.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <returns>A construct that matches the emtpy word.</returns>
+    public static RegExAst<TSymbol> Empty<TSymbol>() => RegExAst<TSymbol>.Epsilon.Instance;
+
+    /// <summary>
+    /// Creates a node that represents a single literal.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="value">The literal to match.</param>
+    /// <returns>A construct that matches <paramref name="value"/>.</returns>
+    public static RegExAst<TSymbol> Literal<TSymbol>(TSymbol value) => new RegExAst<TSymbol>.Literal(value);
+
+    /// <summary>
+    /// Creates a node that represents a range of literals.
+    /// </summary>
+    /// <typeparam name="TSymbol">The symbol type.</typeparam>
+    /// <param name="invert">True, if the ranges should be inverted.</param>
+    /// <param name="intervals">The literal intervals.</param>
+    /// <returns>A construct that matches any of the values covered by <paramref name="intervals"/> if
+    /// <paramref name="invert"/> is false, othervise it matches any value not covered by
+    /// <paramref name="intervals"/>.</returns>
+    public static RegExAst<TSymbol> LiteralRange<TSymbol>(
+        bool invert,
+        IEnumerable<Interval<TSymbol>> intervals) => new RegExAst<TSymbol>.LiteralRange(invert, intervals);
+
+    private static RegExAst<TSymbol> ConstructFolded<TSymbol>(
+        IEnumerable<RegExAst<TSymbol>> elements,
+        Func<RegExAst<TSymbol>, RegExAst<TSymbol>, RegExAst<TSymbol>> folder)
+    {
+        var enumerator = elements.GetEnumerator();
+        if (!enumerator.MoveNext()) return Empty<TSymbol>();
+        var first = enumerator.Current;
+        while (enumerator.MoveNext()) first = folder(first, enumerator.Current);
+        return first;
     }
 }
