240952582c626a27a99beeaddcc11d3958694d48
Lenkefi Péter <lenkefi.peti@gmail.com>
2022-04-07 14:58:34 +0200

    Added a visualizer project for SynKit

---
+++ Sources/SynKit/Tools/Visualizer/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Yoakke.SynKit.Visualizer;
+
+internal sealed class Program
+{
+    internal static void Main(string[] args)
+    {
+        var app = BuildApplication(args);
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+    }
+
+    private static WebApplication BuildApplication(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+
+        return builder.Build();
+    }
+}
