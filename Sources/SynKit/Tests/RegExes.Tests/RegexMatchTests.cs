e516cf54d8d2262625c4a237ded47a29d3b92650
Lenkefi Péter <lenkefi.peti@gmail.com>
2022-04-07 11:29:59 +0200

    Update RegexMatchTests.cs

--- Sources/SynKit/Tests/RegExes.Tests/RegexMatchTests.cs
+++ Sources/SynKit/Tests/RegExes.Tests/RegexMatchTests.cs
@@ -26,23 +26,21 @@ public sealed class RegexMatchTests
     [InlineData(@"(ab)*", "ab")]
     [InlineData(@"(ab)*", "abababab")]
     [InlineData(@"[[:xdigit:]]*", "123ABCDEFabcdef0123456789")]
+    [InlineData(@"[[:^xdigit:]]*", "ghijkl")]
     [Theory]
     public void MatchesNfaDfaMindfa(string regex, string text)
     {
         var settings = new RegExSettings();
         var pcreAst = PcreParser.Parse(settings, regex);
         var rawRegexAst = pcreAst.ToPlainRegex(settings);
-        var nfa = new Nfa<int, char>();
-
-        var randomState = StateCreator.Random();
-        // TODO: Ease this with utilities maybe? Have a method that auto-adds start and end?
-        var (start, end) = rawRegexAst.ThompsonsConstruct(nfa, randomState);
-        nfa.InitialStates.Add(start);
-        nfa.AcceptingStates.Add(end);
+        var nfa = RegexToNfa(rawRegexAst);
+        var epsilonlessNfa = RegexToNfa(rawRegexAst);
+        epsilonlessNfa.EliminateEpsilonTransitions();
         var dfa = nfa.Determinize();
         var minDfa = dfa.Minimize();
 
         Assert.True(nfa.Accepts(text));
+        Assert.True(epsilonlessNfa.Accepts(text));
         Assert.True(dfa.Accepts(text));
         Assert.True(minDfa.Accepts(text));
     }
@@ -60,24 +58,33 @@ public sealed class RegexMatchTests
     [InlineData(@"(ab)*", "aba")]
     [InlineData(@"(ab)*", "ababaabab")]
     [InlineData(@"[[:xdigit:]]*", "abcdghx9y9")]
+    [InlineData(@"[[:^xdigit:]]*", "gh1ijklab")]
     [Theory]
     public void DoesNotMatchNfaDfaMindfa(string regex, string text)
     {
         var settings = new RegExSettings();
         var pcreAst = PcreParser.Parse(settings, regex);
         var rawRegexAst = pcreAst.ToPlainRegex(settings);
-        var nfa = new Nfa<int, char>();
-
-        var randomState = StateCreator.Random();
-        // TODO: Ease this with utilities maybe? Have a method that auto-adds start and end?
-        var (start, end) = rawRegexAst.ThompsonsConstruct(nfa, randomState);
-        nfa.InitialStates.Add(start);
-        nfa.AcceptingStates.Add(end);
+        var nfa = RegexToNfa(rawRegexAst);
+        var epsilonlessNfa = RegexToNfa(rawRegexAst);
+        epsilonlessNfa.EliminateEpsilonTransitions();
         var dfa = nfa.Determinize();
         var minDfa = dfa.Minimize();
 
         Assert.False(nfa.Accepts(text));
+        Assert.False(epsilonlessNfa.Accepts(text));
         Assert.False(dfa.Accepts(text));
         Assert.False(minDfa.Accepts(text));
+    }
+
+    private static Nfa<int, char> RegexToNfa(RegExAst<char> regexAst)
+    {
+        var nfa = new Nfa<int, char>();
+        var randomState = StateCreator.Random();
+        // TODO: Ease this with utilities maybe? Have a method that auto-adds start and end?
+        var (start, end) = regexAst.ThompsonsConstruct(nfa, randomState);
+        nfa.InitialStates.Add(start);
+        nfa.AcceptingStates.Add(end);
+        return nfa;
     }
 }
