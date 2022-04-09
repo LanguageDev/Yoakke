7ecfb0f1a6caa7afe559d61e7f79e6d419b9fd74
Lenkefi Péter <lenkefi.peti@gmail.com>
2022-04-09 17:06:03 +0200

    Made the project client-side, working graph rendering

--- Sources/SynKit/Tools/Visualizer/Program.cs
+++ Sources/SynKit/Tools/Visualizer/Program.cs
@@ -1,45 +1,11 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Yoakke.SynKit.Visualizer;
 
-namespace Yoakke.SynKit.Visualizer;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-internal sealed class Program
-{
-    internal static void Main(string[] args)
-    {
-        var app = BuildApplication(args);
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.MapBlazorHub();
-        app.MapFallbackToPage("/_Host");
-
-        app.Run();
-    }
-
-    private static WebApplication BuildApplication(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-        builder.Services.AddServerSideBlazor();
-
-        return builder.Build();
-    }
-}
+await builder.Build().RunAsync();
