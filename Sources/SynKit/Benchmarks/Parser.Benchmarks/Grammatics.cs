a580a469f0e2d36d3f4fa7775c25699173c6b414
WhiteBlackGoose <wbg@angouri.org>
2022-06-14 20:00:54 +0300

    Initial

---
+++ Sources/SynKit/Benchmarks/Parser.Benchmarks/Grammatics.cs
@@ -0,0 +1,83 @@
+// Copyright (c) 2021-2022 Yoakke.
+// Licensed under the Apache License, Version 2.0.
+// Source repository: https://github.com/LanguageDev/Yoakke
+
+using System;
+using Yoakke.SynKit.Lexer;
+using Yoakke.SynKit.Lexer.Attributes;
+using Yoakke.SynKit.Parser.Attributes;
+
+namespace Yoakke.SynKit.Parser.Benchmarks;
+
+public enum TokenType
+{
+    [Error] Error,
+    [End] End,
+    [Ignore] [Regex(Regexes.Whitespace)] Whitespace,
+
+    [Token("(")] OpenParen,
+    [Token(")")] CloseParen,
+
+    [Token("+")] Add,
+    [Token("-")] Sub,
+    [Token("*")] Mul,
+    [Token("/")] Div,
+    [Token("%")] Mod,
+    [Token("^")] Exp,
+
+    [Token(";")] Semicol,
+
+    [Regex(Regexes.IntLiteral)] IntLit,
+}
+
+[Lexer(typeof(TokenType))]
+public partial class Lexer
+{
+    public List<Token<TokenType>> LexAll()
+    {
+        var list = new List<Token<TokenType>>();
+        while (true)
+        {
+            var token = Next();
+            list.Add(token);
+            if (token.Kind == TokenType.End) break;
+        }
+        return list;
+    }
+}
+
+[Parser(typeof(TokenType))]
+public partial class Parser
+{
+    [Rule("program: expression ';'")]
+    public static int Program(int n, IToken _) => n;
+
+    [Right("^")]
+    [Left("*", "/", "%")]
+    [Left("+", "-")]
+    [Rule("expression")]
+    public static int BinOp(int a, IToken op, int b) => op.Text switch
+    {
+        "^" => (int)Math.Pow(a, b),
+        "*" => a * b,
+        "/" => a / b,
+        "%" => a % b,
+        "+" => a + b,
+        "-" => a - b,
+        _ => throw new NotImplementedException(),
+    };
+
+    [Rule("expression : '(' expression ')'")]
+    public static int Grouping(IToken _1, int n, IToken _2) => n;
+
+    [Rule("expression : IntLit")]
+    public static int IntLit(IToken token) => int.Parse(token.Text);
+
+    public void Synchronize()
+    {
+        for (; this.TokenStream.TryPeek(out var t) && t.Text != ";"; this.TokenStream.Consume(1))
+        {
+        }
+        this.TokenStream.Consume(1);
+    }
+}
