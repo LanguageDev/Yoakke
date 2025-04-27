8752befaa5e935abe46e4958a56b66a28cf77906
Andrii Kurdiumov <kant2002@gmail.com>
2025-04-27 08:22:59 +0500

    Add iterator tests for ParseErrorElementDictionary

--- Sources/SynKit/Tests/Parser.Tests/ParseErrorTests.cs
+++ Sources/SynKit/Tests/Parser.Tests/ParseErrorTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 // Source repository: https://github.com/LanguageDev/Yoakke
 
+using System.Linq;
 using Xunit;
 
 namespace Yoakke.SynKit.Parser.Tests;
@@ -81,5 +82,34 @@ public class ParseErrorTests
         Assert.True(result.Elements["expression"].Expected.Contains("^"));
         Assert.Equal(1, result.Elements["other_expression"].Expected.Count);
         Assert.True(result.Elements["other_expression"].Expected.Contains("^"));
+    }
+
+    [Fact]
+    public void EnumerateWhenSingleElement()
+    {
+        var firstError = new ParseError("^", null, 12, "expression");
+
+        var result = firstError.Elements.Single();
+
+        Assert.Equal("expression", result.Key);
+        Assert.Equal("expression", result.Value.Context);
+        Assert.True(result.Value.Expected.Contains("^"));
+    }
+
+    [Fact]
+    public void EnumerateTwoElements()
+    {
+        var firstError = new ParseError("^", null, 12, "expression");
+        var secondError = new ParseError("^", null, 12, "other_expression");
+        var mergedError = firstError | secondError;
+
+        var result = mergedError.Elements.ToList();
+
+        Assert.Equal("expression", result[0].Key);
+        Assert.Equal("expression", result[0].Value.Context);
+        Assert.True(result[0].Value.Expected.Contains("^"));
+        Assert.Equal("other_expression", result[1].Key);
+        Assert.Equal("other_expression", result[1].Value.Context);
+        Assert.True(result[1].Value.Expected.Contains("^"));
     }
 }
